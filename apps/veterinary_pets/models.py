f37ba432a9bed7a8126d4168c04f902a2ce3e5ec
DeveloperMateo <mateo212705@gmail.com>
2025-03-24 17:52:42 -0500

    First model pets

--- apps/veterinary_pets/models.py
+++ apps/veterinary_pets/models.py
@@ -1,3 +1,8 @@
-from django.db import models
-
-# Create your models here.
+from djongo import models
+ 
+class Pet(models.Model):
+     name = models.CharField(max_length=100)
+     age = models.IntegerField()
+     species = models.CharField(max_length=50)
+     owner = models.CharField(max_length=100)
+     born_date = models.DateField()
