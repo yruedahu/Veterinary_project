f78350f13d7056f17dd38f72ad36c2f0b87c8c74
DeveloperMateo <mateo212705@gmail.com>
2025-03-30 21:47:36 -0500

    Update users aplication

--- apps/veterinary_users/views.py
+++ apps/veterinary_users/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def users_home(request):
-    return HttpResponse("¡Veterinary pets está funcionando!")
+    return render(request, 'veterinary_users/users_home.html')
