c1b92f40fe571f15af549bb0dcbcf145a4ac798b
Edwin Gacha <edwintorxv@yahoo.es>
2025-03-30 21:15:44 -0500

    Creacion modelos y avance en Crud

--- apps/veterinary_clinic/views.py
+++ apps/veterinary_clinic/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import HistoriaClinicaForm
-
-def clinic_home(request):
-    return HttpResponse("¡Veterinary Clinic está funcionando!")
+from .models import HistoriaClinica
 
 def nueva_historia_clinica(request):
-    form = HistoriaClinicaForm()  # Instancia del formulario
+    form = HistoriaClinicaForm()
+    
+    if request.method == "POST":
+        form = HistoriaClinicaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Historia clínica guardada con éxito.")
+
     return render(request, 'veterinary_clinic/historia_clinica.html', {'form': form})
 
+def listar_historias(request):
+    historias = HistoriaClinica.objects.all()
+    return render(request, 'veterinary_clinic/listado_historias.html', {'historias': historias})
