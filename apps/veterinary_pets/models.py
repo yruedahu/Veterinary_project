40e0b7e18f26c013fffe60d590b417c4c3de41bc
natalia <natsil220119@gmail.com>
2025-03-30 21:48:18 -0500

    Actualización en el módulo de Mascotas

--- apps/veterinary_pets/models.py
+++ apps/veterinary_pets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Especie(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
 # Create your models here.
+class Mascota(models.Model):
+    nombre_propietario = models.CharField(max_length=100)
+    nombre_mascota = models.CharField(max_length=100)
+    especie = models.ForeignKey(Especie, on_delete=models.CASCADE)
+    edad = models.IntegerField()
+    sintomas = models.TextField()
+    foto = models.ImageField(upload_to='mascotas/', null=True, blank=True)
