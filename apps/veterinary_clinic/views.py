b8852c4797babffe2f4d792b3d928dac644f9420
Edwin Gacha <edwintorxv@yahoo.es>
2025-03-31 19:17:18 -0500

    Desacople models

--- apps/veterinary_clinic/views.py
+++ apps/veterinary_clinic/views.py
@@ -3,17 +3,17 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import HistoriaClinicaForm
 from .models import HistoriaClinica
+from .forms.historia_clinica_form import HistoriaClinicaForm
 
 def nueva_historia_clinica(request):
-    form = HistoriaClinicaForm()
-    
+    form = HistoriaClinicaForm(request.POST or None)
+
     if request.method == "POST":
-        form = HistoriaClinicaForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "Historia clínica guardada con éxito.")
-
-    return render(request, 'veterinary_clinic/historia_clinica.html', {'form': form})
+    
+    return render(request, "veterinary_clinic/historia_clinica.html", {"form": form})
 
 def listar_historias(request):
     historias = HistoriaClinica.objects.all()
