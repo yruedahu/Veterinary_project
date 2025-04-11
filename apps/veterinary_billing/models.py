08bcc37e545f8c89307baef7a347c0f7f93cee87
Luis sanchez <sanchezgarzonluisenrique@gmail.com>
2025-04-11 16:42:43 -0500

    Cambios elaborados en billing

--- apps/veterinary_billing/models.py
+++ apps/veterinary_billing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Factura(models.Model):
+    nombre_cliente = models.CharField(max_length=100)
+    mascota = models.CharField(max_length=100)
+    servicio = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Factura - {self.nombre_cliente} - {self.mascota}"
--- apps/veterinary_billing/views.py
+++ apps/veterinary_billing/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Factura
+from .forms import FacturaForm
 from django.http import HttpResponse
 
 def billing_home(request):
     return HttpResponse("¡Veterinary Billing está funcionando!")
+
+def listar_facturas(request):
+    facturas = Factura.objects.all()
+    return render(request, 'veterinary_billing/facturas.html', {'facturas': facturas})
+
+def crear_factura(request):
+    if request.method == 'POST':
+        form = FacturaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_facturas')
+    else:
+        form = FacturaForm()
+    return render(request, 'veterinary_billing/form_factura.html', {'form': form})
+
+def editar_factura(request, pk):
+    factura = get_object_or_404(Factura, pk=pk)
+    if request.method == 'POST':
+        form = FacturaForm(request.POST, instance=factura)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_facturas')
+    else:
+        form = FacturaForm(instance=factura)
+    return render(request, 'veterinary_billing/form_factura.html', {'form': form})
+
+def eliminar_factura(request, pk):
+    factura = get_object_or_404(Factura, pk=pk)
+    factura.delete()
+    return redirect('listar_facturas')
+
