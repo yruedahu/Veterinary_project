7cbfbbc28abbb7dc56702f34af7fcdb26bf6afa6
natalia <natsil220119@gmail.com>
2025-05-19 18:39:05 -0500

    Cambios mascotas

--- config/settings.py
+++ config/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': 'veterinary_db',
@@ -110,7 +110,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
         }
     }
 }
- """
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -152,4 +151,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
